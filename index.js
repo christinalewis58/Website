06d1971cdae05141f77c7540f431b10bdb4b3cb5
Christina Lewis <clewis13@ufl.edu>
2023-07-16 12:39:01 -0400

    Added links to sidebar and body copy placeholder

--- index.js
+++ index.js
@@ -97,20 +97,20 @@ document.addEventListener("DOMContentLoaded", function () {
   //TypeError: null is not an object (evaluating 'randomButton.addEventListener')
 });
 
-//Fetch blog posts from mockable.io
+// //Fetch blog posts from mockable.io
 
-const response = await fetch("http://demo3933962.mockable.io/posts", { method: "GET" });
-const posts = await response.json()
-console.log(posts)
+// const response = await fetch("http://demo3933962.mockable.io/posts", { method: "GET" });
+// const posts = await response.json()
+// console.log(posts)
 
-//Show blog post content 
-posts.forEach((post)=>{
-  document.querySelector(".blog").innerHTML += `
-  <article>
-  <h2>${post.title}</h2>
-  <p>${post.author}</p>
-  <p>${post.date}</p>
-  <p>${post.msg}</p>
-  </article>`
-})
+// //Show blog post content 
+// posts.forEach((post)=>{
+//   document.querySelector(".blog").innerHTML += `
+//   <article>
+//   <h2>${post.title}</h2>
+//   <p>${post.author}</p>
+//   <p>${post.date}</p>
+//   <p>${post.msg}</p>
+//   </article>`
+// })
 
